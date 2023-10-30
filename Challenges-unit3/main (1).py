22b244b2e63abb2baa4a09a30345931c00285b6d
subinjose2004 <145641271+subinjose2004@users.noreply.github.com>
2023-10-30 10:06:49 +0530

    Add files via upload

---
+++ Challenges-unit3/main (1).py
@@ -0,0 +1,17 @@
+def linear_search_product(product_list, target_product):
+  indices = []
+  for i, product in enumerate(product_list):
+      if product == target_product:
+          indices.append(i)
+  return indices
+
+# Test the function
+products = ["Apple", "Banana", "Orange", "Apple", "Mango", "Apple"]
+target_product = "Apple"
+
+result = linear_search_product(products, target_product)
+
+if result:
+  print(f"The product '{target_product}' was found at indices: {result}")
+else:
+  print(f"The product '{target_product}' was not found in the list.")
